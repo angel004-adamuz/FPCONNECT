62ed755d5a01d4d69a405e67bbfd0878590be14a
angel004-adamuz <angelamilcobacho4@gmail.com>
2026-05-12 16:31:23 +0200

    Añadido soporte desktop con Electron

---
+++ electron.cjs
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, 'public/favicon.ico'),
+    title: 'FPConnect',
+    show: false,
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    win.loadURL('https://fpconnect.vercel.app/');
+  }
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
